0364504146db52c2cec734daa281b4a19e957dc2
peichhorn <peichhor@web.de>
2011-05-29 23:59:43 +0200

    cleanup

--- src/eclipse/lombok/eclipse/handlers/ast/TypeBuilder.java
+++ src/eclipse/lombok/eclipse/handlers/ast/TypeBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Â© 2011 Philipp Eichhorn
+ * Copyright © 2011 Philipp Eichhorn
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
