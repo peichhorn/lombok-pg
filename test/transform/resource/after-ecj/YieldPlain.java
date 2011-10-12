c1be7dd073426d90ed4225194d1881398d55b5ed
Philipp Eichhorn <peichhor@web.de>
2011-10-12 22:46:30 +0200

    some progress on fixing up yield for eclipse

--- test/transform/resource/after-ecj/YieldPlain.java
+++ test/transform/resource/after-ecj/YieldPlain.java
@@ -43,7 +43,7 @@ class YieldPlain {
         while (true)          switch ($state) {
           case 0 : ;
               $next = "A String";
-              $state = (+ 1);
+              $state = 1;
               return true;
           case 1 : ;
           default : ;
@@ -92,22 +92,22 @@ class YieldPlain {
           case 1 : ;
               if ((! b))
                   {
-                    $state = (+ 3);
+                    $state = 3;
                     continue ;
                   }
               $next = "foo";
-              $state = (+ 2);
+              $state = 2;
               return true;
           case 2 : ;
-              $state = (+ 4);
+              $state = 4;
               continue ;
           case 3 : ;
               $next = "bar";
-              $state = (+ 4);
+              $state = 4;
               return true;
           case 4 : ;
               b = (! b);
-              $state = (+ 1);
+              $state = 1;
               continue ;
           case 5 : ;
           default : ;
@@ -160,21 +160,21 @@ class YieldPlain {
               b = 1;
           case 1 : ;
               $next = a;
-              $state = (+ 2);
+              $state = 2;
               return true;
           case 2 : ;
               c = (a + b);
               if ((! (c < 0)))
                   {
-                    $state = (+ 3);
+                    $state = 3;
                     continue ;
                   }
-              $state = (+ 4);
+              $state = 4;
               continue ;
           case 3 : ;
               a = b;
               b = c;
-              $state = (+ 1);
+              $state = 1;
               continue ;
           case 4 : ;
           default : ;
@@ -229,17 +229,17 @@ class YieldPlain {
           case 1 : ;
               if ((! (b >= 0)))
                   {
-                    $state = (+ 3);
+                    $state = 3;
                     continue ;
                   }
               $next = a;
-              $state = (+ 2);
+              $state = 2;
               return true;
           case 2 : ;
               c = (a + b);
               a = b;
               b = c;
-              $state = (+ 1);
+              $state = 1;
               continue ;
           case 3 : ;
           default : ;
@@ -298,17 +298,17 @@ class YieldPlain {
           case 1 : ;
               if ((! (b >= 0)))
                   {
-                    $state = (+ 3);
+                    $state = 3;
                     continue ;
                   }
               $next = a;
-              $state = (+ 2);
+              $state = 2;
               return true;
           case 2 : ;
               c = (a + b);
               a = b;
               b = c;
-              $state = (+ 1);
+              $state = 1;
               continue ;
           case 3 : ;
           default : ;
@@ -323,7 +323,7 @@ class YieldPlain {
     class $YielderComplexForeach implements java.util.Iterator<String>, java.lang.Iterable<String> {
       private Object object;
       private Class<?> c;
-      private java.util.Iterator $objectIter;
+      private @java.lang.SuppressWarnings("all") java.util.Iterator $objectIter;
       private int $state;
       private boolean $hasNext;
       private boolean $nextDefined;
@@ -366,36 +366,36 @@ class YieldPlain {
           case 1 : ;
               if ((! $objectIter.hasNext()))
                   {
-                    $state = (+ 6);
+                    $state = 6;
                     continue ;
                   }
               object = (Object) $objectIter.next();
               if ((! (object instanceof Class<?>)))
                   {
-                    $state = (+ 4);
+                    $state = 4;
                     continue ;
                   }
               c = (Class<?>) object;
               $next = "A String";
-              $state = (+ 2);
+              $state = 2;
               return true;
           case 2 : ;
               $next = c.getName();
-              $state = (+ 3);
+              $state = 3;
               return true;
           case 3 : ;
-              $state = (+ 6);
+              $state = 6;
               continue ;
           case 4 : ;
               $next = object.toString();
-              $state = (+ 5);
+              $state = 5;
               return true;
           case 5 : ;
-              $state = (+ 1);
+              $state = 1;
               continue ;
           case 6 : ;
               $next = "Another String";
-              $state = (+ 7);
+              $state = 7;
               return true;
           case 7 : ;
           default : ;
@@ -445,41 +445,41 @@ class YieldPlain {
         while (true)          switch ($state) {
           case 0 : ;
               $next = "Another String";
-              $state = (+ 1);
+              $state = 1;
               return true;
           case 1 : ;
               if ((! objects.hasNext()))
                   {
-                    $state = (+ 6);
+                    $state = 6;
                     continue ;
                   }
               object = objects.next();
               if ((! (object instanceof Class<?>)))
                   {
-                    $state = (+ 4);
+                    $state = 4;
                     continue ;
                   }
               c = (Class<?>) object;
               $next = "A String";
-              $state = (+ 2);
+              $state = 2;
               return true;
           case 2 : ;
               $next = c.getName();
-              $state = (+ 3);
+              $state = 3;
               return true;
           case 3 : ;
-              $state = (+ 6);
+              $state = 6;
               continue ;
           case 4 : ;
               $next = object.toString();
-              $state = (+ 5);
+              $state = 5;
               return true;
           case 5 : ;
-              $state = (+ 1);
+              $state = 1;
               continue ;
           case 6 : ;
               $next = "Another String";
-              $state = (+ 7);
+              $state = 7;
               return true;
           case 7 : ;
           default : ;
