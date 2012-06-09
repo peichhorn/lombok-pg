d8d18b9b4ede8103c83b69c78badecae6199a99e
Philipp Eichhorn <peichhor@web.de>
2012-06-09 12:33:09 +0200

    trivial

--- src/core/lombok/Position.java
+++ src/core/lombok/Position.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2010-2011 Philipp Eichhorn
+ * Copyright © 2010-2012 Philipp Eichhorn
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
